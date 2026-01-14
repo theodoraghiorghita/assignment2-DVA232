c4712db840f23ee332fc22467faac103627622f7
Theodora <theodoraghiorghita@gmail.com>
2026-01-14 14:32:52 +0200

    Update currency_utils.dart

--- lib/utils/currency_utils.dart
+++ lib/utils/currency_utils.dart
@@ -8,20 +8,3 @@ String currencyCodeToCountryName(String currency) {
 String currencyToCountryCode(String currency) {
   return cc.currencyToCountryCode[currency] ?? "unknown";
 }
-
-Widget flagForCurrency(String currency, {double size = 32}) {
-  final code = currencyToCountryCode(currency).toLowerCase();
-
-  if (code == "unknown") {
-    return SizedBox(width: size, height: size);
-  }
-
-  return Image.asset(
-    'icons/flags/png/$code.png',
-    package: 'country_icons',
-    width: size,
-    height: size,
-    errorBuilder: (context, error, stack) =>
-        SizedBox(width: size, height: size),
-  );
-}
